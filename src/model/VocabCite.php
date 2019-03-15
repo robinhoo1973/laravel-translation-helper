66a0e967975ee30561bc731b796fb5b0bc1c3b9e
Robin Hoo <RobinHoo1973@users.noreply.github.com>
2019-03-15 21:52:40 +0800

    Correct The Line Number

--- src/model/VocabCite.php
+++ src/model/VocabCite.php
@@ -33,18 +33,18 @@ class VocabCite extends Model
     public function sweep()
     {
         $line = explode("\n", file_get_contents($this->file));
-        $line = $line[$this->line] ?? '';
+        $line = $line[$this->line - 1] ?? '';
         if ($line) {
             $terms = call_user_func_array(
                 'array_merge',
                 array_map(
-                    function($u) {
+                    function ($u) {
                         return ["localize('{$u->term}')", 'localize("' . $u->term . '")'];
                     },
                     $this->terms()->get()->all()
                 )
             );
-            $matched = array_filter(array_map(function($u) use ($line) {
+            $matched = array_filter(array_map(function ($u) use ($line) {
                 return strpos($line, $u);
             }, $terms), 'strlen');
 
