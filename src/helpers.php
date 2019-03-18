a928bb23304f87b4cca15feb8e8cb5398c52b152
Robin Hoo <RobinHoo1973@users.noreply.github.com>
2019-03-19 07:39:34 +0800

    Add Helper Function

--- src/helpers.php
+++ src/helpers.php
@@ -4,6 +4,29 @@ use TopviewDigital\TranslationHelper\Model\VocabTerm;
 use TopviewDigital\TranslationHelper\Model\VocabCite;
 use TopviewDigital\TranslationHelper\Service\Translation;
 
+if (! function_exists('array_sort_value')) {
+    function array_sort_value($array, $mode = SORT_LOCALE_STRING)
+    {
+        // SORT_REGULAR - compare items normally (don't change types)
+        // SORT_NUMERIC - compare items numerically
+        // SORT_STRING - compare items as strings
+        // SORT_LOCALE_STRING - compare items as strings, based on the current locale.
+        // It uses the locale, which can be changed using setlocale()
+        // SORT_NATURAL - compare items as strings using "natural ordering" like natsort()
+        // SORT_FLAG_CASE
+
+        if (! is_array($array)) {
+            $array = method_exists($array, 'toArray') ? $array->toArray() : (array) $array;
+        }
+        // \Locale::setDefault(str_replace('-', '_', \App::getLocale()));
+        $keys = array_keys($array);
+        $vals = array_values($array);
+        array_multisort($vals, $mode, $keys);
+
+        return array_combine($keys, $vals);
+    }
+}
+
 if (!function_exists('is_json')) {
     function is_json($string)
     {
